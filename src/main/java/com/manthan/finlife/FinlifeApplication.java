5594fa6c3473a6e777bd251d2310388ea13d55e1
manthan <manthanchauhan@indiagold.co>
2023-06-04 17:19:45 +0530

    Initial commit

---
+++ src/main/java/com/manthan/finlife/FinlifeApplication.java
@@ -0,0 +1,13 @@
+package com.manthan.finlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinlifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinlifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manthan/finlife/FinlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manthan.finlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinlifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
