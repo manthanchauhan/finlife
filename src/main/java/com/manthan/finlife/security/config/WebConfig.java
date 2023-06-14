0aeae85e370dc415c8acd1181717b5a51b317526
manthan <manthanchauhan@indiagold.co>
2023-06-15 00:36:35 +0530

    Added CORS config

---
+++ src/main/java/com/manthan/finlife/security/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.manthan.finlife.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH");
+    }
+}
